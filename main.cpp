678fd208395ce408e5bd74f7e8af1e1434823db2
jjastad <jastadj@gmail.com>
2016-01-12 14:09:49 -0800

    gridlos wip

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 #include <SFML\Graphics.hpp>
 
@@ -82,12 +83,54 @@ std::vector< sf::Vector2f> gridLine(float x1, float y1, float x2, float y2)
             return gridsOnLine;
         }
     }
+
+    //get equation of a line
+    // y = mx + b
+    // b = y - mx
+    // x = (b - y)/m
+    float m = (y2 - y1) / (x2 - x1);
+    float b = y1 - m*x1;
+
+    //debug
+    std::cout << "y = " << m << "*x + " << b << std::endl;
+
+    //horizontal collection of grids
+    for(int i = x1; i <= x2; i++)
+    {
+        gridsOnLine.push_back( sf::Vector2f( i, m*float(i) + b));
+    }
+
+    float y_low = y1;
+    float y_high = y2;
+    if(y_low > y_high)
+    {
+        y_low = y2;
+        y_high = y1;
+    }
+
+
+    //vertical collection of grids
+    for(int i = y_low; i <= y_high; i++)
+    {
+        if(m == 0) gridsOnLine.push_back( sf::Vector2f(x1, i));
+        else
+        {
+            std::cout << (b-i)/m << "," << i << std::endl;
+            gridsOnLine.push_back( sf::Vector2f( -floor((b-i)/m), i) );
+        }
+    }
+
     //add point 1 and point 2
     //gridsOnLine.push_back( sf::Vector2f(x1, y1));
     //gridsOnLine.push_back( sf::Vector2f(x2, y2));
 
     return gridsOnLine;
 
+}
+
+std::vector< sf::Vector2f> gridLine(sf::Vector2f p1, sf::Vector2f p2)
+{
+    return gridLine(p1.x, p1.y, p2.x, p2.y);
 }
 
 int main(int argc, char *argv[])
@@ -97,11 +140,27 @@ int main(int argc, char *argv[])
 
     bool quit = false;
 
+    sf::Vector2f p1(2.5,2.5);
+    sf::Vector2f p2(6.5,6.5);
+
     while(!quit)
     {
         sf::Event event;
 
         screen->clear();
+
+        //capture mouse position
+        sf::Vector2f mousePos = sf::Vector2f( sf::Mouse::getPosition(*screen));
+
+        //convert mouse position into grid coordinates
+        sf::Vector2f mousePosGrid = mousePos;
+        mousePosGrid.x = floor(mousePosGrid.x / TILE_SIZE);
+        mousePosGrid.y = floor(mousePosGrid.y / TILE_SIZE);
+
+        std::cout << "mouse: " << mousePos.x << "," << mousePos.y << "  -  " << mousePosGrid.x << "," << mousePosGrid.y << std::endl;
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)) p1 = mousePosGrid;
+        else if(sf::Mouse::isButtonPressed(sf::Mouse::Right)) p2 = mousePosGrid;
 
         while(screen->pollEvent(event) )
         {
@@ -109,6 +168,13 @@ int main(int argc, char *argv[])
             else if(event.type == sf::Event::KeyPressed)
             {
                 if(event.key.code == sf::Keyboard::Escape) quit = true;
+            }
+            else if(event.type == sf::Event::MouseButtonPressed)
+            {
+                if(event.mouseButton.button == sf::Mouse::Left)
+                {
+
+                }
             }
         }
 
@@ -126,7 +192,7 @@ int main(int argc, char *argv[])
         }
 
         //draw grids on a line
-        std::vector< sf::Vector2f> mygrids = gridLine(2,2, 2,19);
+        std::vector< sf::Vector2f> mygrids = gridLine(p1, p2);
         for(int i = 0; i < int(mygrids.size()); i++)
         {
             drawGrid( screen, mygrids[i].x, mygrids[i].y, sf::Color::Green, sf::Color::Transparent);
